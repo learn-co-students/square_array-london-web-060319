4944d6c8bbc1a58182c1dadf3e813dd1ff212f42
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-05-16 16:20:52 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
-end
+def square_array(numbers)
+  # your code here	  
+  new_numbers=[]
+  numbers.each do |number|
+    new_numbers << number**2
+  end
+ new_numbers
+end 
